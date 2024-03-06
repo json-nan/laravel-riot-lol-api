968e08f03b612f116b3ac1351ee3cfd331674429
Jasson López <jasson.lopez@elaniin.com>
2024-03-05 23:42:34 -0600

    :construction: Add enums

---
+++ src/Enums/Platform.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace JsonNaN\LaravelRiotLolApi\Enums;
+
+enum Platform: string
+{
+    case BR1 = 'br1';
+    case EUN1 = 'eun1';
+    case EUW1 = 'euw1';
+    case JP1 = 'jp1';
+    case KR = 'kr';
+    case LA1 = 'la1';
+    case LA2 = 'la2';
+    case NA1 = 'na1';
+    case OC1 = 'oc1';
+    case PH2 = 'ph2';
+    case RU = 'ru';
+    case SG2 = 'sg2';
+    case TH2 = 'th2';
+    case TR1 = 'tr1';
+    case TW2 = 'tw2';
+    case VN2 = 'vn2';
+}
---
+++ src/Enums/Region.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace JsonNaN\LaravelRiotLolApi\Enums;
+
+enum Region: string
+{
+    case AMERICAS = 'americas';
+    case ASIA = 'asia';
+    case ESPORTS = 'esports';
+    case EUROPE = 'europe';
+    case SEA = 'sea';
+}
